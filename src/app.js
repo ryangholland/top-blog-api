624d3ad9c1e1914112e96e11e08b78d35279816c
ryangholland <rghcode@gmail.com>
2025-02-20 14:35:01 -0500

    Enable CORS Requests

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const postRoutes = require("./routes/postRoutes");
 const commentRoutes = require("./routes/commentRoutes");
 const authRoutes = require("./routes/authRoutes");
 
+app.use(cors);
 app.use(express.json());
 
 app.use("/posts", postRoutes);
