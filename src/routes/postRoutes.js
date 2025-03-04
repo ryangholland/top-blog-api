01bb9c9eef8949afdb5fb6bf6ebd2a96e25204e6
ryangholland <rghcode@gmail.com>
2025-03-04 09:02:01 -0500

    Organize post routes

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -3,12 +3,16 @@ const router = express.Router();
 const postController = require("../controllers/postController");
 const authenticate = require("../middleware/authMiddleware");
 
+// Public
 router.get("/", postController.getAllPosts);
 router.get("/recent", postController.getRecentPosts);
 router.get("/random", postController.getRandomPost);
 router.get("/:id", postController.getPostById);
-router.post("/", authenticate, postController.createPost);
-router.put("/:id", authenticate, postController.updatePost);
-router.delete("/:id", authenticate, postController.deletePost);
+
+// Protected
+router.use(authenticate);
+router.post("/", postController.createPost);
+router.put("/:id", postController.updatePost);
+router.delete("/:id", postController.deletePost);
 
 module.exports = router;
